85eb85508431b1cf096b257e7b5010819510c99d
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-27 22:08:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ def find_valid_calls(array)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   array.any? do |element|
-    element
+    element == valid_calls
  end
 end
